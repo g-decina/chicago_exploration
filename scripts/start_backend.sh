ce1d6307245e5671febf77c694ca3eb5c3bded59
gdecina <147255487+gdecina@users.noreply.github.com>
2025-11-25 09:49:14 -0600

    fix: ports now correctly configured

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-if [ ! -f 'data/current/chicago_licenses_master.geojson' ]; then
-    echo 'First run detected. Processing raw data...'
-    python -m data.extraction
-else
-    echo 'Data found. Starting API...'
-fi
-
 PORT=${PORT:-8000}
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT --reload
+exec uvicorn app.main:app --host 0.0.0.0 --port '$PORT'
