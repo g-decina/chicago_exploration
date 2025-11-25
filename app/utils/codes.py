cd841cc97e16d6318ac048d78f3e17f70d04925c
gdecina <147255487+gdecina@users.noreply.github.com>
2025-11-24 21:03:18 -0600

    fix: /codes endpoint now working (really)

--- app/utils/codes.py
+++ app/utils/codes.py
@@ -5,4 +5,4 @@ from app.config import *
 INDEX = pd.read_csv(DATA_DIR / 'cluster_index.csv', dtype=str)
 
 def list_codes(gdf) -> list[str]:
-    return gdf[ACT_CLEAN].str.unique()
+    return gdf[ACT_CLEAN].sort_values().unique().tolist()
